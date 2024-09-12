1b476d7dcac370a3ef04272961891628fbe7f42f
M. Naufal Faqih <naufalfaqih443@gmail.com>
2024-09-12 10:40:38 +0700

    Refactor scroll behavior and navigation styling

--- DPW/assets/js/main.js
+++ DPW/assets/js/main.js
@@ -58,6 +58,8 @@ document.addEventListener("DOMContentLoaded", function () {
         top: targetPosition,
         behavior: "smooth",
       });
+
+      document.querySelector(".nav-list").classList.remove("active");
     });
   });
 });
